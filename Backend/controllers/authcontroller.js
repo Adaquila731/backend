a616879295a5db7822d0bad6f27442606a8f730f
shaqsdean <mulandidean@gmail.com>
2025-05-24 23:46:32 +0300

    Edited the name

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -7,7 +7,7 @@ const generateToken = (user) => {
 
 exports.register = async (req, res) => {
   try {
-    const { name, email, password, location } = req.body;
+    const { firstName, lastName, email, password, location } = req.body;
 
     const existing = await User.findOne({ email });
     if (existing) return res.status(400).json({ message: 'Email already registered' });
